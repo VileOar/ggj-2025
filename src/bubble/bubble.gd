1ef529727559e8ace1dd51feddf3633b48c8fe55
Franciscorp <franciscorp1994@hotmail.com>
2025-01-25 17:41:56 +0000

    bubble

--- src/bubble/bubble.gd
+++ src/bubble/bubble.gd
@@ -1,22 +1,21 @@
-extends RigidBody2D
 class_name Bubble
+extends RigidBody2D
+
+signal big_bubble_collision
+
+const MAX_SCALE_LIMIT = 1.65
+const MIN_SCALE_LIMIT = 0.5
 
 const SPEED = 300.0
-const SPEED_STOP_THRESHOLD = 5
-
-const DRAG_RESISTANCE = 0.9999
-const ON_COLLISION_DIRECTION_VARIATION = 0.25
 
 const COLLISION_VELOCITY_THRESHOLD = 60.0
 const COLLISION_SCALE_PERCENTAGE_THRESHOLD = 0.25
+const ON_JOIN_SCALE_DIVISION_FACTOR = 3
 
 @onready var bubble: Sprite2D = $bubble
 @onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
 
 var bubble_scale : Vector2 = Vector2(1.0, 1.0)
-
-
-var velocity
 var rng = RandomNumberGenerator.new()
 
 
@@ -50,39 +49,19 @@ func _set_random_scale() -> void:
 	# Apply scale to children
 	bubble.scale = bubble_scale
 	collision_shape_2d.scale = bubble_scale
-	print("scale applied: ", bubble_scale)
 
 
-func _physics_process(delta: float) -> void:
-	#var collision_info = move_and_collide(velocity * delta)
-	#_apply_drag()
-	#
-	## ON collision detection, changes direction with slight variable 
-	#if collision_info:
-		#_change_direction_slighly(collision_info)
-		#_on_collision_slow_down()
-	pass
-
-
-func _on_collision_slow_down() -> void:
-	#print("slow down")
-	velocity.x *= 0.99
-	velocity.y *= 0.99
+func _update_scale(new_body_scale: Vector2) -> void: 
+	var new_scale: Vector2 = abs(bubble_scale) + abs(new_body_scale / ON_JOIN_SCALE_DIVISION_FACTOR)
+	print("Update Scale Ori: ", bubble_scale, " Add: ", (new_body_scale / 3), "Total:", new_scale)
 	
-	#
-#func _apply_drag() -> void:
-##	stop speed after threshold
-	#if abs(velocity.x) < SPEED_STOP_THRESHOLD && abs(velocity.y) < SPEED_STOP_THRESHOLD:
-		#velocity.x = 0
-		#velocity.y = 0
-	#else:
-##		apply drag normally
-		#velocity.x *= DRAG_RESISTANCE
-		#velocity.y *= DRAG_RESISTANCE
-		##print(velocity.x)
-		##print(velocity.y)
-		#pass
-	
+	if new_scale.x >= MAX_SCALE_LIMIT:
+		new_scale = Vector2(MAX_SCALE_LIMIT, MAX_SCALE_LIMIT)
+		
+	# Apply scale to children
+	bubble.scale = new_scale 
+	collision_shape_2d.scale = new_scale
+
 	
 func _integrate_forces(state: PhysicsDirectBodyState2D) -> void:
 	var velocity := state.get_linear_velocity()
@@ -92,29 +71,58 @@ func _integrate_forces(state: PhysicsDirectBodyState2D) -> void:
 		var collision_normal := state.get_contact_local_normal(collider_index)
 	
 
-func _change_direction_slighly(collision_info) -> void:
-	var new_bounce_vector = collision_info.get_normal()
-	new_bounce_vector.x += rng.randf_range(-ON_COLLISION_DIRECTION_VARIATION, ON_COLLISION_DIRECTION_VARIATION)
-	new_bounce_vector.y += rng.randf_range(-ON_COLLISION_DIRECTION_VARIATION, ON_COLLISION_DIRECTION_VARIATION)
-	velocity = velocity.bounce(new_bounce_vector.normalized())
+#func _change_direction_slighly(collision_info) -> void:
+	#var new_bounce_vector = collision_info.get_normal()
+	#new_bounce_vector.x += rng.randf_range(-ON_COLLISION_DIRECTION_VARIATION, ON_COLLISION_DIRECTION_VARIATION)
+	#new_bounce_vector.y += rng.randf_range(-ON_COLLISION_DIRECTION_VARIATION, ON_COLLISION_DIRECTION_VARIATION)
+	#velocity = velocity.bounce(new_bounce_vector.normalized())
 
 
-#	TODO work
+func _is_slow_collision() -> bool:
+#	Detects if FASTER than threshold
+	if abs(linear_velocity.x) > COLLISION_VELOCITY_THRESHOLD || abs(linear_velocity.y) > COLLISION_VELOCITY_THRESHOLD:
+		return false
+	else:
+		return true
+
+
+func _is_scale_difference_small(body) -> bool:
+	#var scale_difference = bubble_scale.x - body.scale.x 
+	var scale_difference = snappedf(bubble_scale.x, 0.001) - snappedf(body.scale.x , 0.001)
+	#snappedf(bubble_scale.x, 0.001)
+	#snappedf(body.scale.x , 0.001)
+	scale_difference = abs(scale_difference)
+	print("Myself = ", bubble_scale.x)
+	print("Other = ", body.bubble_scale.x)
+	print("Myself = ", snappedf(bubble_scale.x, 0.001))
+	print("Other = ", snappedf(body.scale.x , 0.001))
+	print("scale_difference = ", scale_difference)
+	
+#	Detects if SCALE_DIFFERENCE IS BIGGER than threshold
+	if scale_difference > COLLISION_SCALE_PERCENTAGE_THRESHOLD:
+		big_bubble_collision.emit()
+		return false
+	else:
+		return true
+
+
 func _on_body_entered(body: Node2D) -> void:
 	if body is Bubble:
-		print("bubble collision")
+		#if _is_slow_collision():
+			#return
 		
-		if abs(linear_velocity.x) > COLLISION_VELOCITY_THRESHOLD || abs(linear_velocity.y) > COLLISION_VELOCITY_THRESHOLD:
-			print("Fast collision")
-		else:
-			print("slow collision")
+		if _is_scale_difference_small(body):
+			return
 			
-		var scale_difference = abs(bubble_scale.x - body.scale.x) 
-		print("scale difference = ", scale_difference)
-		if scale_difference > COLLISION_SCALE_PERCENTAGE_THRESHOLD:
-			print("Scale is bigger than threshold")
-		else:
-			print("Scale is smaller than threshold")
+		print("scale difference is valid")
+		
+		if bubble_scale.x >= body.bubble_scale.x:
+			print("BIG")
+			#_update_scale(body.bubble_scale)
+			#body.queue_free()
+		#else:
+			#print("DELETE")
+			#queue_free()
+
+		
 			
-			
-	#print(body.name)
--- src/game_controller.gd
+++ src/game_controller.gd
@@ -1,11 +1,44 @@
+class_name GameController
 extends Node2D
 
 
-# Called when the node enters the scene tree for the first time.
+@export var BUBBLE: PackedScene
+
+var rng = RandomNumberGenerator.new()
+
+
 func _ready() -> void:
-	pass # Replace with function body.
+#	Starts with two bubbles
+	spawn_bubble()
+	spawn_bubble()
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+#	SPAWN BUBBLE
+	if Input.is_action_just_pressed("launch_bubble"):
+		spawn_bubble()
+	
+	
+func _set_bubble_connection(new_bubble) -> void:
+	new_bubble.big_bubble_collision.connect(_join_bubbles)
+	
+	
+func _join_bubbles() -> void:
+	#print("bubbles join")
 	pass
+	
+	
+func spawn_bubble():
+	randomize()
+	var screen_size = get_viewport().get_visible_rect().size
+	
+	#new_bubble.position = get_viewport().get_mouse_position()
+	var new_bubble = BUBBLE.instantiate()
+	_set_bubble_connection(new_bubble)
+	
+#	Random position to spawn
+	new_bubble.position.x = randi_range(0, screen_size.x)
+	new_bubble.position.y = randi_range(0, screen_size.y)
+	
+	# adds to scene
+	add_child(new_bubble)
