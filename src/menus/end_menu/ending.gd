56330defd9ec9f53658e071d554af61cb28a35c6
Franciscorp <franciscorp1994@hotmail.com>
2025-01-26 15:53:28 +0000

    winning condition

--- src/menus/end_menu/ending.gd
+++ src/menus/end_menu/ending.gd
@@ -4,11 +4,26 @@ extends Control
 
 var main_menu_scene : PackedScene
 
+@onready var orange_background: PanelContainer = $OrangeBackground
+@onready var yellow_background: PanelContainer = $YellowBackground
+
+
 
 func _ready():
 	main_menu_scene = load("res://src/menus/MainMenu.tscn")
 	menu_button.button_down.connect(_on_menu_pressed)
 
+func set_winner(is_orange_winner: bool) -> void:
+	yellow_background.visible = !is_orange_winner
+	orange_background.visible = is_orange_winner
+
+func orange_wins() -> void:
+	yellow_background.visible = false
+	orange_background.visible = true
+	
+func yellow_wins() -> void:
+	yellow_background.visible = true
+	orange_background.visible = false
 
 func _on_menu_pressed() -> void:
 	#_play_click_sfx()
