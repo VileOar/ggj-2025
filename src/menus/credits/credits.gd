d5f8ef7bbd1c02baf32d7b86ce51c2359d564048
luischavesdev <115810776+luischavesdev@users.noreply.github.com>
2025-01-29 17:04:30 +0000

    more menu sounds

--- src/menus/credits/credits.gd
+++ src/menus/credits/credits.gd
@@ -5,3 +5,8 @@ extends Control
 
 func _on_back_menu_button_pressed():
 	credits.visible = false
+	AudioManager.play_audio("ButtonAccept")
+
+
+func _on_back_menu_button_mouse_entered():
+	AudioManager.play_audio("ButtonDecline")
--- src/menus/main_menu.gd
+++ src/menus/main_menu.gd
@@ -2,18 +2,18 @@ class_name MainMenu
 extends Control
 
 @onready var start_game_button : Button = %PlayButton
-#@onready var credits = %CreditsButton
-#@onready var how_to_play = %HowToPlayButton
+@onready var credits_button = %CreditsButton
 @onready var exit_button : Button = %ExitButton
+
 @onready var credits: Control = %Credits
 
 @onready var game_scene : PackedScene = preload("res://src/main_scenes/stage.tscn")
 
 
 func _ready():
-# Connects buttons to functions
-	#start_game_button.grab_focus()
+	# Connects buttons to functions
 	start_game_button.button_down.connect(_on_start_pressed)
+	credits_button.button_down.connect(_on_credits_pressed)
 	exit_button.button_down.connect(_on_exit_pressed)
 
 
@@ -24,18 +24,17 @@ func _play_click_sfx() -> void:
 func _play_hover_sfx() -> void:
 	AudioManager.play_audio("ButtonDecline")
 
-# Starts game
+
 func _on_start_pressed() -> void:
 	_play_click_sfx()
 	get_tree().change_scene_to_packed(game_scene)
 
 
-func _on_credits_button_pressed():
+func _on_credits_pressed():
 	_play_click_sfx()
 	credits.visible = true
 	
 	
-# Exists game
 func _on_exit_pressed() -> void:
 	_play_click_sfx()
 	get_tree().quit()
