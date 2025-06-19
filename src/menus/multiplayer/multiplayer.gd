eba56dd357fecb4617d118400b33d394cb0ccd3e
Franciscorp <franciscorp1994@hotmail.com>
2025-06-19 16:14:45 +0100

    Host menu done

---
+++ src/menus/multiplayer/multiplayer.gd
@@ -0,0 +1,19 @@
+extends Control
+
+
+var peer = ENetMultiplayerPeer.new()
+@export var player_scene : PackedScene
+
+func _on_host_pressed():
+	# Por open on windows by default, check open ports with netstat -aon
+	peer.create_server(135)
+	multiplayer.multiplayer_peer = peer
+#	When signal peer_connected received, calls add player
+	multiplayer.peer_connected.connect(_add_player)
+#	adds own player
+	_add_player()
+	
+func _add_player(id = 1) -> void:
+	pass
+	
+	
