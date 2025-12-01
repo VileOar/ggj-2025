23214af9b57eff2b33f229931c860f9c81710ef7
luischavesdev <115810776+luischavesdev@users.noreply.github.com>
2025-12-01 00:45:03 +0000

    New wave vfx and removed exit from pause

--- src/menus/pause/pause_menu.gd
+++ src/menus/pause/pause_menu.gd
@@ -4,7 +4,6 @@ extends Control
 @onready var continue_button: Button = %ContinueButton
 @onready var options_button: Button = %OptionsButton
 @onready var menu_button: Button = %MenuButton
-@onready var exit_button: Button = %ExitButton
 
 @onready var _pause_buttons = %PauseButtons
 @onready var _options_menu: OptionsMenu = %OptionsMenu
@@ -13,18 +12,16 @@ extends Control
 func _ready():
 	_pause_buttons.visible = true
 	_options_menu.visible = false
-	
+
 	# Connects buttons to functions
 	continue_button.button_down.connect(_on_continue_pressed)
 	options_button.button_down.connect(_on_options_pressed)
 	menu_button.button_down.connect(_on_menu_pressed)
-	exit_button.button_down.connect(_on_exit_pressed)
-	
+
 	continue_button.mouse_entered.connect(_on_mouse_entered)
 	options_button.mouse_entered.connect(_on_mouse_entered)
 	menu_button.mouse_entered.connect(_on_mouse_entered)
-	exit_button.mouse_entered.connect(_on_mouse_entered)
-	
+
 
 func _play_click_sfx() -> void:
 	AudioManager.play_audio(Global.Sounds.ACCEPT_UI)
@@ -52,19 +49,15 @@ func _on_options_pressed() -> void:
 	_play_click_sfx()
 	_options_menu.visible = true
 	_pause_buttons.visible = false
-	
-	
+
+
 func _on_menu_pressed() -> void:
 	_play_click_sfx()
 	get_tree().paused = false
 	AudioManager.stop_audio(Global.Sounds.FIGHT_MUSIC)
 	Global.change_scene(Global.Scenes.MAIN_MENU)
-	
 
-func _on_exit_pressed() -> void:
-	get_tree().quit()
-	
-	
+
 func _on_mouse_entered() -> void:
 	AudioManager.play_audio(Global.Sounds.HOVER_UI)
 
