db1ee8cb3371e1b3daf233283dbd3df8b2923277
luischavesdev <115810776+luischavesdev@users.noreply.github.com>
2025-12-09 02:19:20 +0000

    Bubble sfx tweaks

--- src/bubble/bubble_burst.gd
+++ src/bubble/bubble_burst.gd
@@ -1,5 +1,12 @@
 extends AnimatedSprite2D
 
+@onready var _pop_stream: AudioStreamPlayer2D = %PopStream
+
+
+func _ready():
+	var lambda = func(): _pop_stream.play()
+	AudioManager.play_with_delay(Global.Sounds.BUBBLE_POP, 0.07, lambda)
+
 
 func _on_animation_finished() -> void:
 	queue_free()
